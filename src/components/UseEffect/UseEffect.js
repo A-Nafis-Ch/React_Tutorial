9b56b1c43816547bad6e58145a4e0588d24617c0
Nafis <abdullahnafis256@gmail.com>
2025-04-20 17:32:51 +0600

    useEffect component added

--- src/components/UseEffect/UseEffect.js
+++ src/components/UseEffect/UseEffect.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 
+
+
 const UseEffect = (props) => {
+
+  const useEffect = (() => {
+     alert("I will be called in every render!");
+  })
 
     const clickHandler = () => {
 
@@ -12,9 +18,16 @@ const UseEffect = (props) => {
 
     }
 
+    const colorHandler = () => {
+
+      setCurrentColor(color[Math.floor(Math.random() * color.length)]);
+    }
+
     
 
     const [count, setCount] = useState(0);
+    const [color] = useState(['red', 'green', 'yellow'])
+    const [currentColor, setCurrentColor] = useState(color[0])
 
     
 useEffect(() => {
@@ -29,6 +42,11 @@ useEffect(() => {
       <h1>{count}</h1>
 
       <button onClick={clickHandler}>+</button>
+
+      <h1>{currentColor}</h1>
+      <h1>Color: {props.color}</h1>
+
+      <button onClick={colorHandler}>Change Color</button>
     </div>
   )
 }
